aa81fe519ba3adc745246f94557addbc7cb29c3a
Mellio <kllmmc23@gmail.com>
2023-01-24 23:24:24 -0700

    add fix to avoid overflow

--- BinarySearch/Binary-Search.py
+++ BinarySearch/Binary-Search.py
@@ -1,56 +1,57 @@
 from typing import List
 
+# [1, 2, 3, 4, 5, 6]
+#  l
+#                 r
+#        m
+#      t
+# --------------------------
+# [1, 2, 3, 4, 5, 6]
+#           l
+#                 r
+#        m
+#           t
+# --------------------------
+# [1, 2, 3, 4, 5, 6, 7]
+#  l
+#                    r
+#           m
+# ------------------------------------------
+# O(log(n)) time
+# ------------------------------------------
+# n = size => 8 => [1,2,3,4,5,6,7,8]
+# T(n) = T(n/2) + C
+#     8/2^1 = 4  => 1    [1,2,3,4
+#     8/2^2 = 2  => 2    [1,2
+#     8/2^3 = 1  => 3    [1
+# ----n/2^k = 1 => n = 2^k => logn = k =>
+#                                                     Left                                  Right
+#                                                                          n = 8                                k
+#                                                                    [1,2,3,4,5,6,7,8]                          0
+#                                                                /                       \
+#                                                       [1,2,3,4                        5,6,7,8]                1
+#                                                   [1,2        3,4                  5,6       7,8]             2
+#                                                 [1    2     3     4              5     6    7     8]          3
+#
+#
 
-    # [1, 2, 3, 4, 5, 6]
-    #  l
-    #                 r
-    #        m
-    #      t
-    # --------------------------
-    # [1, 2, 3, 4, 5, 6]
-    #           l
-    #                 r
-    #        m
-    #           t
-    # --------------------------
-    # [1, 2, 3, 4, 5, 6, 7]
-    #  l
-    #                    r
-    #           m
-    # ------------------------------------------
-    # O(log(n)) time
-    # ------------------------------------------
-    # n = size => 8 => [1,2,3,4,5,6,7,8]
-    # T(n) = T(n/2) + C
-    #     8/2^1 = 4  => 1    [1,2,3,4
-    #     8/2^2 = 2  => 2    [1,2
-    #     8/2^3 = 1  => 3    [1
-    # ----n/2^k = 1 => n = 2^k => logn = k =>
-    #                                                     Left                                  Right
-    #                                                                          n = 8                                k
-    #                                                                    [1,2,3,4,5,6,7,8]                          0
-    #                                                                /                       \
-    #                                                       [1,2,3,4                        5,6,7,8]                1
-    #                                                   [1,2        3,4                  5,6       7,8]             2
-    #                                                 [1    2     3     4              5     6    7     8]          3
-    #
-    #
+# Using Master Theorem T(n) = O(logn)
+#
+#  Assuming already sorted
+#
+#
+#
+# O(logn) Space
+# Recursive Solution
 
-    # Using Master Theorem T(n) = O(logn)
-    #
-    #  Assuming already sorted
-    #
-    #
-    #
-    # O(logn) Space
-    # Recursive Solution
+
 class Solution1:
     def search(self, nums: List[int], target: int) -> int:
         def BinarySearch(nums, left, right, target):
             # base case
             if left > right:
                 return -1
-            mid = (left + right) // 2
+            mid = left + (right - left) // 2  # fixed to avoid overflow
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
@@ -65,6 +66,8 @@ class Solution1:
 # O(log(n)) time
 # O(1) space
 # Iterative Solution
+
+
 class Solution2:
     def search(self, nums: List[int], target: int) -> int:
         length = len(nums)
@@ -72,7 +75,7 @@ class Solution2:
         left = 0
 
         while (left <= right):
-            mid = (right + left)//2
+            mid = left + (right - left) // 2
             if (target == nums[mid]):
                 return mid
             elif target > nums[mid]:
