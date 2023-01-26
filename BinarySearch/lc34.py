416ff6f6089cf814f56be18cc2c16331b7ddeb0d
Mellio <kllmmc23@gmail.com>
2023-01-25 23:00:35 -0700

    add erase description

--- BinarySearch/lc34.py
+++ BinarySearch/lc34.py
@@ -1,30 +1,3 @@
-'''
-week1 Problem day 3: (Since we missed one on Monday)
-https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
-34. Find First and Last Position of Element in Sorted Array
-tag: Binary search
-Medium
-Facebook Amazon Bloomberg
-Given an array of integers nums sorted in non-decreasing order, 
-find the starting and ending position of a given target value.
-If target is not found in the array, return [-1, -1].
-You must write an algorithm with O(log n) runtime complexity.
-Example 1:
-Input: nums = [5,7,7,8,8,10], target = 8
-Output: [3,4]
-Example 2:
-Input: nums = [5,7,7,8,8,10], target = 6
-Output: [-1,-1]
-Example 3:
-Input: nums = [], target = 0
-Output: [-1,-1]
-Constraints:
-0 <= nums.length <= 105
--109 <= nums[i] <= 109
-nums is a non-decreasing array.
--109 <= target <= 109
-'''
-
 
 class Solution(object):
 
