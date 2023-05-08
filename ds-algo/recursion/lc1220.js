d63a43d0635f9bf303cf4d0a840ba337c00e3355
Mellio <kllmmc23@gmail.com>
2023-05-07 20:34:04 -0600

    add lc 780

---
+++ ds-algo/recursion/lc1220.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countVowelPermutation = function(n) {
+    // Write your code here
+    /*                                                              build                                        depth
+                                           a        e               i                o          u                  0
+                                           e      ea ei       ia  ie io iu        oi   ou       a                  1
+  
+                                           pseudo code
+                                              
+  
+       */
+  }
+  
